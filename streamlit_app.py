a32fd8a2401ea02387c8be5622e5e46c9ea2a2e7
ChanchalDubeyAtg <105267468+ChanchalDubeyAtg@users.noreply.github.com>
2022-05-27 11:56:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
-#import streamlit
+import streamlit
 streamlit.title("My Mom\'s New Healthy Diner")
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
