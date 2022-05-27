7e52e6ffe98e94d8e389f0aa8bfd1c93db7e0b18
ChanchalDubeyAtg <105267468+ChanchalDubeyAtg@users.noreply.github.com>
2022-05-27 11:34:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ streamlit.write('The user entered',fruit_choice)
 #import requests 
 fruityvice_response = requests.get ("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-#pip3 install --upgrade pip --user
+pip3 install --upgrade pip --user
 pip install -r https://raw.githubusercontent.com/snowflakedb/snowflake-connector-python/v2.7.6/tested_requirements/requirements_36.reqs
 streamlit.stop()
 
